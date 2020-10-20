c0e241818fc3cb443607d3da490f100679729d68
sbristol@usgs.gov <sbristol@usgs.gov>
2020-10-20 12:59:46 -0600

    Fixed issue in WikiData entity retrieval to handle bad requests

--- examples/isaid_scripts/cache_sb_directory.py
+++ examples/isaid_scripts/cache_sb_directory.py
@@ -6,11 +6,18 @@ import os
 
 cmd_sb = pylinkedcmd.pylinkedcmd.Sciencebase()
 
-usgs_staff = cmd_sb.get_active_usgs_staff()
+#usgs_staff = cmd_sb.get_active_usgs_staff()
+usgs_staff = cmd_sb.get_staff_by_email(["mtmartin@usgs.gov","meaton@usgs.gov"])
+
+sbid_list_for_query = str([i["identifier_sbid"]
+                           for i in usgs_staff]).replace('"', "'").replace("[", "(").replace("]", ")")
 
 df_people = pd.DataFrame(usgs_staff)
 df_people = df_people.drop_duplicates(subset='identifier_sbid', keep="first")
-df_people.loc[df_people['identifier_orcid'].duplicated(), 'identifier_orcid'] = np.NaN
+try:
+    df_people.loc[df_people['identifier_orcid'].duplicated(), 'identifier_orcid'] = np.NaN
+except:
+    pass
 df_people.loc[df_people['identifier_email'].duplicated(), 'identifier_email'] = np.NaN
 
 pg_user = os.environ["PG_USER"]
@@ -22,7 +29,7 @@ pg_db = os.environ["PG_DB"]
 pg_engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
 with pg_engine.connect() as con:
-    con.execute("DELETE FROM people")
+    con.execute(f"DELETE FROM people where identifier_sbid IN {sbid_list_for_query}")
     con.close()
 
 df_people.to_sql(
